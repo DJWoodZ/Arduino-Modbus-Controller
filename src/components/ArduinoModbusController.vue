5e48461c8c6c329030c0c4a988044b96ed49a361
DJWoodZ <DJWoodZ@users.noreply.github.com>
2025-06-09 20:33:01 +0100

    Remove unnecessary CSS classes

--- src/components/ArduinoModbusController.vue
+++ src/components/ArduinoModbusController.vue
@@ -206,7 +206,7 @@
   </v-container>
 
   <v-alert v-else density="compact" type="error" variant="outlined">Sorry, your browser does not support the Web Serial API. Please use a
-    <a class="text-decoration-none font-weight-bold text-error" href="https://caniuse.com/web-serial" rel="noopener noreferrer" target="_blank">compatible browser</a>.</v-alert>
+    <a class="text-error" href="https://caniuse.com/web-serial" rel="noopener noreferrer" target="_blank">compatible browser</a>.</v-alert>
 
 </template>
 
